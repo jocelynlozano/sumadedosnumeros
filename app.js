27047db356266c45366022c858df51d6fb34c6d6
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-08-25 15:02:37 -0500

    subiendo ejercicios

---
+++ app.js
@@ -0,0 +1,3 @@
+var numero1 = parseInt(prompt ("Ingrese su primer numero"));
+var numero2 = parseInt(prompt ("Ingrese su segundo numero"));
+	window.alert("este es tu resultado " + (numero1 + numero2));
